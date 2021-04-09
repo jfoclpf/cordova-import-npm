8020018344899242fb93e686d880d86a97fe9ae5
jfoclpf <joao.pimentel.ferreira@gmail.com>
2021-04-09 22:57:08 +0200

    update

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,23 +1,24 @@
 #!/usr/bin/env node
 
-// const fs = require('fs')
+const fs = require('fs')
 // const fse = require('fs-extra')
-// const path = require('path')
+const path = require('path')
 const colors = require('colors/safe')
 
-// const xml2js = require('xml2js')
-// const parser = new xml2js.Parser()
+const xml2js = require('xml2js')
+const parser = new xml2js.Parser()
 
-// const isTest = process.argv[2] === '--test'
+const isTest = process.argv[2] === '--test'
 console.log('Postinstall script running on ' + colors.cyan(process.env.INIT_CWD))
 process.exitCode = 0
 
-/*
-var configXmlFileName = isTest ? path.join(__dirname, '..', 'test', 'config.xml') : 'config.xml'
+const configXmlFileName = isTest
+  ? path.join(__dirname, '..', 'test', 'config.xml')
+  : path.join(process.env.INIT_CWD, 'config.xml')
 
 console.log(`Reading file ${colors.cyan(configXmlFileName)}`)
 
-var rawdata = fs.readFileSync(configXmlFileName, 'utf8')
+const rawdata = fs.readFileSync(configXmlFileName, 'utf8')
 parser.parseString(rawdata, function (err, result) {
   if (err) {
     console.log(colors.red('Invalid config.xml file'))
@@ -31,4 +32,3 @@ parser.parseString(rawdata, function (err, result) {
   }
   console.log(result)
 })
-*/
